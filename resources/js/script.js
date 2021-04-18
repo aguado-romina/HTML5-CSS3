ff6d6a07afa1b93bae82d415d93252a574a7b205
Romina Aguado <aguado.romi@gmail.com>
2021-04-18 15:09:32 -0500

    Adding animations on scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -50,4 +50,9 @@ $(document).ready(function () {
       }
     });
   });
+
+  /* Animations on scroll */
+  $(".js--wp-1").waypoint(function (direction) {
+    $(".js--wp-1").addClass("animated fadeIn");
+  });
 });
