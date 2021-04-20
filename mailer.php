7ec559a9114ea77c6c5e4842d30f605692c0dcbc
Romina Aguado <aguado.romi@gmail.com>
2021-04-20 14:53:44 -0500

    Using PHP to make our form work

--- mailer.php
+++ mailer.php
@@ -8,7 +8,7 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://www.webdesigncourse.co/omnifood/index.php?success=-1#form");
+        header("Location: https://aguado-romina.github.io/Omnifood//index.php?success=-1#form");
         exit;
     }
 
